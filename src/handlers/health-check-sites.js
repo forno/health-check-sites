6e115bb39f566c7fb39c7e574a15a4f47bf0f1fc
FORNO <forno@xmaho.link>
2022-04-07 01:23:11 +0000

    Add show list of targets on invoked
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/handlers/health-check-sites.js
+++ src/handlers/health-check-sites.js
@@ -5,18 +5,20 @@ const targets = process.env.TARGETS.split(',')
 //! WEBHOOK_URLはSlackのWebhook URL
 const webhookurl = process.env.WEBHOOK_URL
 
+const send2Slack = async text => fetch(webhookurl, {
+  method: 'post',
+  body: JSON.stringify({ text })
+})
+
 /**
  * A Lambda function that check health of some sites
  */
 export const healthCheckSites = async () => {
+  await send2Slack(`health-check-sites: On invoked, I show targets list
+${targets.join('\n')}`)
   await Promise.all(targets.map(async (target) => // 全てのURLに対して
     fetch(target).catch(e => // GETを送信して問題があれば
-      fetch( // slackに通知を送信する
-        webhookurl,
-        {
-          method: 'post',
-          body: JSON.stringify({ text: `Error: url; ${target} detail: ${e}` })
-        })
+      send2Slack(`<!here> Error: url; ${target} detail; ${e}`) // slackに通知する
     )
   ))
 }
