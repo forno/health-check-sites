e56306558d3679aa6d6507cb3a93e0a471328707
FORNO <forno@xmaho.link>
2022-04-12 00:17:45 +0000

    Add abort controller for fetch timeout
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/handlers/health-check-sites.js
+++ src/handlers/health-check-sites.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import { AbortController } from 'node-abort-controller'
 
 //! TARGETSはカンマ区切りの検証対象URL
 const targets = process.env.TARGETS.split(',')
@@ -16,9 +17,14 @@ const send2Slack = async text => fetch(webhookurl, {
 export const healthCheckSites = async () => {
   await send2Slack(`health-check-sites: On invoked, I show targets list
 ${targets.join('\n')}`)
-  await Promise.all(targets.map(async (target) => // 全てのURLに対して
-    fetch(target).catch(e => // GETを送信して問題があれば
-      send2Slack(`<!here> Error: url; ${target} detail; ${e}`) // slackに通知する
-    )
-  ))
+  const controller = new AbortController()
+  const signal = controller.signal
+  await Promise.all([
+    setTimeout(() => controller.abort(), 500), // timeoutの設定
+    Promise.all(targets.map(async (target) => // 全てのURLに対して
+      fetch(target, { signal }).catch(e => // GETを送信して問題があれば
+        send2Slack(`<!here> Error: url; ${target} detail; ${e}`) // slackに通知する
+      )
+    ))
+  ])
 }
